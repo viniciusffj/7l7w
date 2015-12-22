765cdbef698c187ac8ad32dca8372d36231bb6dc
Vinicius Fernandes <viniciusffj@gmail.com>
2015-12-22 20:45:05 -0200

    Prolog - Finishing day 1

---
+++ prolog/day1/books.pl
@@ -0,0 +1,8 @@
+book_author(seven_language, bruce).
+book_author(refactoring, martin_fowler).
+book_author(dsl, martin_fowler).
+book_author(pofeaa, martin_fowler).
+book_author(tdd, kent_beck).
+book_author(xp_explained, kent_beck).
+book_author(clean_code, uncle_bob).
+book_author(ppp, uncle_bob).
---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,13 @@
+musician_instrument(champs, bass).
+musician_instrument(chorao, vocal).
+musician_instrument(thiago, guitar).
+musician_instrument(cartola, guitar).
+musician_instrument(martinho, pandeiro).
+musician_instrument(nelson, cuica).
+
+musician_genre(champs, rock).
+musician_genre(chorao, rock).
+musician_genre(thiago, rock).
+musician_genre(cartola, samba).
+musician_genre(martinho, samba).
+musician_genre(nelson, samba).
